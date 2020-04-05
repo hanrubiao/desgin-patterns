34e9bb9a527e77d90d0b98a8ebda1d863cb4f5dc
dam <15250991567@139.com>
2020-04-05 20:14:27 +0800

    singleton create

---
+++ src/anytxn/jtx/trick/singleton/service/Singleton.java
@@ -0,0 +1,11 @@
+package anytxn.jtx.trick.singleton.service;
+
+/**
+ * @ClassName Singleton.java
+ * @Author trick
+ * @Version 1.0.0
+ * @Description 单例类
+ * @CreateTime 2020年04月05日 20:13:00
+ */
+public class Singleton {
+}
